ba5642fc9ca49a9bd395c71130702cf9ee46161f
yeah2223 <yeah13261632@gmail.com>
2025-03-31 01:41:52 +0900

    Honoでbackendを構築

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 type BookManager = {
   id: number;
@@ -8,20 +9,73 @@ type BookManager = {
 };
 
 const books: BookManager[] = [
-  { id: 1, name: "Book 1", status: "在庫あり" },
-  { id: 2, name: "Book 2", status: "貸出中" },
-  { id: 3, name: "Book 3", status: "返却済" },
+  { id: 1, name: "React", status: "在庫あり" },
+  { id: 2, name: "JavaScript", status: "貸出中" },
+  { id: 3, name: "Next.js", status: "返却済" },
 ];
 
 const app = new Hono();
+
+app.use(
+  "/*",
+  cors({
+    origin: ["http://localhost:3000"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 3600,
+    credentials: true,
+  })
+);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
 app.get("/books", (c) => {
+  const query = c.req.query();
+  const keyword = query.keyword;
+
+  if (keyword) {
+    const filteredBooks = books.filter((book) => book.name.includes(keyword));
+    return c.json(filteredBooks);
+  }
   return c.json(books);
 });
+
+app.post("/books", async (c) => {
+  const body = await c.req.json();
+  const name = body["name"];
+
+  if (!name) {
+    return c.json({ error: "書名が入力されていません" });
+  }
+
+  const newBook = {
+    id: books.length + 1,
+    name: name,
+    status: "在庫あり",
+  };
+  books.push(newBook);
+
+  return c.json(newBook);
+});
+
+app.put("/books/:id", async (c) => {
+  const id = c.req.param("id");
+  const body = await c.req.json();
+  const status = body["status"];
+
+  const book = books.find((book) => book.id === Number(id));
+
+  if (!book) {
+    return c.json({ error: "書籍が見つかりません" });
+  }
+  book.status = status;
+
+  return c.json(book);
+});
+
 serve(
   {
     fetch: app.fetch,
